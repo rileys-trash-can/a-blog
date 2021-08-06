70f701a08f29ae31776e7e35c2ba7151b4572c62
derzombiiie <derz@elidragon.com>
2021-08-06 02:19:41 +0200

    Added new social icon
    Very importain change!
    pls respect
    and check out
    my tiktiok!

--- html/js/share.js
+++ html/js/share.js
@@ -1,12 +1,13 @@
 $(document).ready(() => {
     sharemenu = new metaentry(undefined,"Share",".content",{"content":
 `Don't know why you would, but here you can find me elsewhere on the Net:<br \\>
-<a href="https://github.com/derzombiiie"><img class="socialicon" src="/static//icon/github.png" \\></a>
+<a href="//github.com/derzombiiie"><img class="socialicon" title="pls click tiktok icon!" src="/static/icon/github.png" \\></a>
+<a href="//www.youtube.com/watch?v=dQw4w9WgXcQ"><img title="100% legit" class="socialicon" src="/static/icon/tiktok.png" \\></a>
 <small>(yea thats it)</small><br \\><br \\>
 oh yea the title of this box: go share my page: 
 <a class="share" href="mailto:?subject=DerZombiiies%20totally%20cool%20website!&body=Hi%2C%0AI%20just%20found%20this%20on%20the%20interwebz%3A%20derzombiiie.com">Email</a>,
 <a class="share" href="whatsapp://send?text=Hi%2C%0AI%20just%20found%20this%20on%20the%20interwebz%3A%20derzombiiie.com">WhatsApp</a>,
-<a class="share" href="https://twitter.com/intent/tweet?text==Check%20derzombiiies%20website!%0A%40%20derzombiiie.com">Twitter</a>,
+<a class="share" href="//twitter.com/intent/tweet?text==Check%20derzombiiies%20website!%0A%40%20derzombiiie.com">Twitter</a>,
 Copy: <input value="derzombiiie.com" \\>`
     })
 })
