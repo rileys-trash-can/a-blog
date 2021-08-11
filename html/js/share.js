849414e8a55d000f8fc0150cd3541315eb3b5489
derzombiiie <derz@elidragon.com>
2021-08-12 01:38:32 +0200

    added titles to share links

--- html/js/share.js
+++ html/js/share.js
@@ -8,9 +8,9 @@ function displayshare() {
 <a href="//www.youtube.com/watch?v=dQw4w9WgXcQ"><img title="100% legit" class="socialicon" src="/static/icon/tiktok.png" \\></a>
 <small>(yea thats it lol)</small><br \\><br \\>
 oh yea the title of this box: go share this page with...
-<a class="share" href="mailto:?subject=DerZombiiies%20totally%20cool%20website!&body=Hi%2C%0AI%20just%20found%20this%20on%20the%20interwebz%3A%20${shareurl}">Email</a>,
-<a class="share" href="whatsapp://send?text=Hi%2C%0AI%20just%20found%20this%20on%20the%20interwebz%3A%20${shareurl}">WhatsApp</a>,
-<a class="share" href="https://twitter.com/intent/tweet?text==Check%20derzombiiies%20website!%0A%40%20${shareurl}">Twitter</a>,
+<a title="good old fashioned email!"             class="share" href="mailto:?subject=DerZombiiies%20totally%20cool%20website!&body=Hi%2C%0AI%20just%20found%20this%20on%20the%20interwebz%3A%20${shareurl}">Email</a>,
+<a title="i dont use it; works though (i think)" class="share" href="whatsapp://send?text=Hi%2C%0AI%20just%20found%20this%20on%20the%20interwebz%3A%20${shareurl}">WhatsApp</a>,
+<a title="twitter, pls url be not to long!" class="share" href="https://twitter.com/intent/tweet?text==Check%20derzombiiies%20website!%0A%40%20${shareurl}">Twitter</a>,
 Copy: <input value="${shareurl}" \\>`
     })
 }
